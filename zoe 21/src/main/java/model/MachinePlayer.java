c708b1f202944d8ab998a1a7d8678507c6b81885
skyler-ashe <5kyl3r@proton.me>
2024-05-09 22:28:03 +0200

    new

--- zoe 21/src/main/java/model/MachinePlayer.java
+++ zoe 21/src/main/java/model/MachinePlayer.java
@@ -8,33 +8,57 @@ public final class MachinePlayer extends Player {
     public String getName() {
         return "Computer";
     }
-    private int difficulty;
 
+    private int difficulty;
     public void setDifficulty(int difficulty) {
         this.difficulty = difficulty;
     }
-    // dummy method that @skyler and @yana can override ;)
+    public int searchDepth() {
+        return switch (difficulty) {
+            case 1 -> 1;
+            case 2 -> 2;
+            case 3 -> 3;
+            default -> 1;
+        };
+    }
+
     public int makeMove(int currentRoundNr, Stack<Integer> currentStack) {
-        if (difficulty == 1) {
-            if (currentRoundNr < 4) {
-                return ((int) (Math.random() * 9 + 1));
-            } else {
-                return ((int) (Math.random() * 10));
+
+        Position root = new Position(currentStack, currentRoundNr, true, -1);
+        int bestEval = minimax(root, searchDepth(), true);
+        for (Position child : root.getChildren()) {
+            if (child.getEvaluation() == bestEval) {
+                return child.getMoveMade();
             }
-        } else if (difficulty == 2) {
-            if (currentRoundNr < 4) {
-                return ((int) (Math.random() * 9 + 1));
-            } else {
-                return ((int) (Math.random() * 10));
-            }
-        } else if (difficulty == 3) {
-            if (currentRoundNr < 4) {
-                return ((int) (Math.random() * 9 + 1));
-            } else {
-                return ((int) (Math.random() * 10));
-            }
+        }
+
+        return -1;
+    }
+
+    public int minimax(Position pos, int depth, boolean machinesMove) {
+    // machinesMove is somewhat equivalent to 'maximizingPlayer', since the COM is always maximizing
+
+        int eval = pos.getEvaluation();
+        if (depth == 0 || eval >= 1000 || eval <= -1000) {
+            return eval;
         } else {
-            return ((int) (Math.random() * 9 + 1));
+            pos.generateChildren();
+        }
+
+        if (machinesMove) {
+            int maxEval = -1000;
+            for (Position child : pos.getChildren()) {
+                eval = minimax(child, depth - 1, false);
+                maxEval = Math.max(maxEval, eval);
+            }
+            return maxEval;
+        } else {
+            int minEval = 1000;
+            for (Position child : pos.getChildren()) {
+                eval = minimax(child, depth - 1, true);
+                minEval = Math.min(minEval, eval);
+            }
+            return minEval;
         }
     }
 }
---
+++ zoe 21/src/main/java/model/Position.java
@@ -0,0 +1,91 @@
+package model;
+
+import javafx.geometry.Pos;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class Position {
+
+    private final Stack<Integer> positionStack;
+    private final int positionRoundNr;
+    private final boolean machinesMove;
+
+    private final int moveMade;
+    public int getMoveMade() {
+        return moveMade;
+    }
+
+    private final int evaluation;
+    public int getEvaluation() {
+        return evaluation;
+    }
+
+    public Position(Stack<Integer> currentStack, int roundNr, boolean machinesMove, int moveMade) {
+        this.positionStack = currentStack;
+        this.positionRoundNr = roundNr;
+        this.machinesMove = machinesMove;
+        this.moveMade = moveMade;
+
+        int nr1 = currentStack.pop();
+        int nr2 = currentStack.pop();
+        boolean stackFull = currentStack.size() >= 6;
+        boolean canCombine = roundNr >= 4 && currentStack.size() > 1;
+
+        if (nr1 >= 12 && stackFull || nr1 + nr2 >= 21 && canCombine) {
+            this.evaluation = 1000;
+        } else if (nr1 == 11 && stackFull && nr1 + nr2 < 21 || nr1 + nr2 == 20 && stackFull) {
+            this.evaluation = -1000;
+        } else {
+            this.evaluation = 0;
+        }
+    }
+
+    private final Set<Position> children = new HashSet<>();
+    public Set<Position> getChildren() {
+        return children;
+    }
+
+//    public Position generateChild(int move, Position parent) {
+//
+//        Stack<Integer> childStack = parent.positionStack;
+//        int childRound = parent.machinesMove ? parent.positionRoundNr + 1: parent.positionRoundNr;
+//
+//        if (move == 0 && childStack.size() > 1 && childRound >= 4) {
+//            childStack.push(childStack.pop() + childStack.pop());
+//        } else if (move <= 9 && move >= 1) {
+//            if (childStack.size() == 6) {
+//                int stackNr1 = childStack.pop();
+//                childStack.push(stackNr1 + move);
+//            } else if (!childStack.isEmpty() && childStack.size() < 6) {
+//                childStack.push(move);
+//            }
+//        }
+//
+//        return new Position(childStack, childRound, !parent.machinesMove, move);
+//    }
+
+    public void generateChildren() {
+        for (int move = 0; move < 10; move++) {
+            Stack<Integer> childStack = this.positionStack;
+            int childRound = this.machinesMove ? this.positionRoundNr + 1: this.positionRoundNr;
+
+            if (move == 0 && childStack.size() > 1 && childRound >= 4) {
+                childStack.push(childStack.pop() + childStack.pop());
+            } else if (move >= 1) {
+                if (childStack.size() == 6) {
+                    int stackNr1 = childStack.pop();
+                    childStack.push(stackNr1 + move);
+                } else if (!childStack.isEmpty() && childStack.size() < 6) {
+                    childStack.push(move);
+                }
+            }
+
+            Position child = new Position(childStack, childRound, !this.machinesMove, move);
+            children.add(child);
+        }
+    }
+
+
+}
