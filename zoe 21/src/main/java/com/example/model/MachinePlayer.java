e9537fead95b54d3868306a2b0e01dc0f3cbb774
skyler-ashe <5kyl3r@proton.me>
2024-05-12 18:13:21 +0200

    new

--- zoe 21/src/main/java/com/example/model/MachinePlayer.java
+++ zoe 21/src/main/java/com/example/model/MachinePlayer.java
@@ -2,6 +2,10 @@ package com.example.model;
 
 import java.util.Stack;
 
+// THIS CODE WAS WRITTEN WITH EXPANDABILITY IN MIND
+// IT CAN BE SIMPLIFIED MUCH MORE AND BE DESIGNED MORE ELEGANTLY AT THIS STAGE
+// HOWEVER, THE WAY IT IS WRITTEN IS FULLY FUNCTIONAL AND LEAVES SPACE FOR LARGER IMPROVEMENTS TO BE IMPLEMENTED EASIER
+
 public final class MachinePlayer extends Player {
     @Override
     public String getName() {
@@ -9,46 +13,103 @@ public final class MachinePlayer extends Player {
     }
 
     private static int depth;
-    public static void setDifficulty(int difficulty) {
-        System.out.println("Difficulty: " + difficulty);
-        if (difficulty == 1) {
-            depth = 2;
-        } else if (difficulty == 2) {
-            depth = 4;
-        } else if (difficulty == 3) {
-            depth = 8;
+    private static int difficulty;
+    public static void setDifficulty(int diff) {
+        System.out.println("Difficulty: " + diff);
+
+        // easy (1) & medium (2) AIs don't simulate moves, only check for 'instant winning' moves
+        // the depth is only set for diff 3, which is the only one that actually searches.
+        if (diff == 1) {
+            difficulty = diff;
+            depth = 0;
+        } else if (diff == 2) {
+            difficulty = diff;
+            depth = 0;
+        } else if (diff == 3) {
+            difficulty = diff;
+            depth = 11;
         } else {
-            depth = 4;
+            // "Easter-Egg"-Difficulty if choosing a difficulty is skipped - literally unbeatable AI
+            // depth is set to 0 since this AI is based on a specific strategy whether than looking ahead
+            difficulty = 4;
+            depth = 0;
+            System.out.println("\n\n !!! ACTIVATED UNBEATABLE MATH-MODE !!! \n\n");
         }
-        System.out.println("Depth: " + depth);
     }
 
     public static int makeMove(int currentRoundNr, Stack<Integer> currentStack) {
 
-        // don't calculate scores in the first two rounds, play a random number instead
-        if (currentRoundNr < 3) {
-            return ((int) (Math.random() * 9 + 1));
+
+
+        // instant reactions
+        if (currentRoundNr <= 3) {
+
+            if (difficulty == 4) {
+                // round 3, on the second player's (the AI's) turn, '1' is the strongest move - therefore the unbeatable AI plays it always
+                return 1;
+            }
+
+            // the other AI's just all play random moves in the first three rounds
+            return (int) (Math.random() * 9 + 1);
+
+        } else if (difficulty == 1) {
+
+            // easy ai (1) has a 70% chance of just outputting a random number and therefore missing a win.
+            if ((int) (Math.random() * 100) <= 70) {
+                return (int) (Math.random() * 10);
+            }
+
+
+        } else if (currentStack.size() >= 2) {
+
+            // if a win/loss is detected inevitably in the next move/round
+            int top = currentStack.peek();
+            // since stack has at least 2 elements and is at least round 4, create sum from top two numbers
+            int sum = top + currentStack.get(currentStack.size() - 2);
+
+            if (sum >= 21) {
+                // win by combining the two top elements to sum
+                return 0;
+            }
+
+            // check if stack is full
+            if (currentStack.size() >= 6) {
+                if (top >= 12) {
+                    // win by adding large enough number on top to exactly reach 21
+                    return 21 - top;
+                }
+
+                if (top == 11 || sum == 20) {
+                    // cannot prevent enemy from winning on their next turn, so just return random
+                    // (in order to skip searching possible positions and save resources)
+                    return (int) (Math.random() * 10);
+                }
+            }
+
         }
 
-        // initialize current position as root-node for algorithm
+        // (only for "Hard" AI (difficulty 3)
+        // if game is yet undetermined, search possible paths (using minimax-algorithm with alpha-beta branch-pruning)
+        // initialize current position as root-node for algorithm ('lastMoveMade' is passed -1 since just as a filler, this variable is not relevant for the root Position)
         Position root = new Position(currentStack, currentRoundNr, true, -1);
-        int highestPossibleScore = minimaxEvaluation(root, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+        // minimax is called from the root position, with 'alpha = -infinity/min', 'beta = infinity/max' and 'maximizingPlayer = true' (since its called from MachinePlayer's perspective)
+        int highestPositionScore = minimaxAlphaBeta(root, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+        // note: depth is always either 0 or 11. there are, of course, many more elegantly seeming solutions here at first
+        // (like defaulting depth to 11 and only letting Hard AI call it) but most of this code was written with expandability in mind.
 
         // iterating over all possible next positions
         for (Position p : root.getPossibleNextPositions()) {
             // if position is among top-rated...
-            if (p.getPositionScore() == highestPossibleScore) {
+            if (p.getPositionScore() == highestPositionScore) {
                 // ...add the move that lead to it to list of best moves
                 root.getHighestRatedMoves().add(p.getLastMoveMade());
 
                 // the reason for this implementation (saving all the best moves to an instance of Position instead of inside this method)
-                // is for a not-yet-implemented expansion where the AI saves the results of calculations to be reused and improve performance
-
-                System.out.println("Move: " + p.getLastMoveMade() + " Eval: " + p.getPositionScore());
+                // is for a not-yet-implemented idea where the AI saves the results of calculations to be reused and improve performance
             }
         }
-        System.out.println("Valid Moves: " + root.getHighestRatedMoves());
 
+        // return one of the moves among the list of best ones randomly
         if (!root.getHighestRatedMoves().isEmpty()) {
             return root.getHighestRatedMoves().get((int) (Math.random() * root.getHighestRatedMoves().size()));
         } else {
@@ -57,43 +118,46 @@ public final class MachinePlayer extends Player {
         }
     }
 
-    public static int minimaxEvaluation(Position pos, int depth, int alphaPruning, int betaPruning, boolean maximizingPlayer) {
-        // Position pos:                current game-state as position object
+    public static int minimaxAlphaBeta(Position pos, int depth, int alpha, int beta, boolean maximizingPlayer) {
+        // Position pos:                current game-state as instance of Position-Class
         // int depth:                   search depth in moves (2 moves = 1 round)
         // boolean maximizingPlayer:    whether the current player aims to maximize or minimize the position's evaluation
-        //                               - P1 aims to maximize
-        //                               - P2 aims to minimize (computer is always P2)
+        //                               - Player 2, the AI, always assumes itself to be the maximizing player
 
         int score = pos.getPositionScore();
         if (depth == 0 || score != 0) {
             // if maximum search depth is reached or an inevitable loss/win is found:
-            // pass position's score upwards
+            // --> pass position's score upwards
             return score;
         } else {
             pos.simulatePossibleMoves();
         }
 
         // search tree for best possible path
-        // iterate over all generated possible next positions
-        // recursively call minimax until score is returned
         if (maximizingPlayer) {
+            // if its MachinePlayer's turn;
             int scoreMax = Integer.MIN_VALUE;
+            // iterate over all generated possible next positions
             for (Position child : pos.getPossibleNextPositions()) {
-                score = minimaxEvaluation(child, depth - 1, alphaPruning, betaPruning, false);
+                // recursively call minimax until score is returned
+                score = minimaxAlphaBeta(child, depth - 1, alpha, beta, false);
+                // check whether it is higher than the previous maximum or alpha-value
                 scoreMax = Math.max(scoreMax, score);
-                alphaPruning = Math.max(alphaPruning, score);
-                if (betaPruning <= alphaPruning) {
+                alpha = Math.max(alpha, score);
+                // determine whether tree-branch is worth exploring (if it even can contain better outcomes for us)
+                if (beta <= alpha) {
                     break;
                 }
             }
             return scoreMax;
         } else {
+            // same as above, but for Player's turns
             int scoreMin = Integer.MAX_VALUE;
             for (Position child : pos.getPossibleNextPositions()) {
-                score = minimaxEvaluation(child, depth - 1, alphaPruning, betaPruning, true);
+                score = minimaxAlphaBeta(child, depth - 1, alpha, beta, true);
                 scoreMin = Math.min(scoreMin, score);
-                betaPruning = Math.max(betaPruning, score);
-                if (alphaPruning <= betaPruning) {
+                beta = Math.min(beta, score);
+                if (beta <= alpha) {
                     break;
                 }
             }
--- zoe 21/src/main/java/com/example/model/Position.java
+++ zoe 21/src/main/java/com/example/model/Position.java
@@ -5,6 +5,10 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.Stack;
 
+// THIS CODE WAS WRITTEN WITH EXPANDABILITY IN MIND
+// IT CAN BE SIMPLIFIED MUCH MORE AND BE DESIGNED MORE ELEGANTLY AT THIS STAGE
+// HOWEVER, THE WAY IT IS WRITTEN IS FULLY FUNCTIONAL AND LEAVES SPACE FOR LARGER IMPROVEMENTS TO BE IMPLEMENTED EASIER
+
 public class Position {
 
     private final Stack<Integer> currentStack;
@@ -25,49 +29,46 @@ public class Position {
         this.roundNumber = roundNr;
         this.isMaximizingPlayer = activePlayerIsMachine;
         this.lastMoveMade = lastMoveMade; // move made (by opponent) leading to this position
-        this.positionScore = calculateScore_heuristic(currentStack, !activePlayerIsMachine);
+        this.positionScore = calculateScore_heuristic(currentStack, activePlayerIsMachine);
     }
 
     // calculates score of current position
-    // if it inevitably/heuristically-predictably leads to a win/loss:
-    //  - MAX/MIN if game is already in end-position
-    //  - +/-1000 if game will end after one move
-    //  - +/-500 if game will end after two moves
+    // if it inevitably/heuristically-predictably leads to a win/loss
     // positive numbers indicate the maximizing player has advantage (and the opposite)
     // if no heuristic evaluation is possible, return 0 as a neutral rating
-    private int calculateScore_heuristic(Stack<Integer> s, boolean max) {
+    private int calculateScore_heuristic(Stack<Integer> s, boolean isMaximizingPlayersTurn) {
+        // looks quite the same to MachinePlayer.makeMove() - this is only called for the scoring-values if no win/loss
+        // can be determined in the next few moves. makeMove gets called *first* for performance-reasons
 
-        if (s.isEmpty()) return 0; // check if stack is empty, return 0 as neutral score if so
-        int top = s.peek(); // since stack has been verified as not empty, get stack's top number
-
-        if (top >= 21) {
-            // check if position is already final (already >= 21)
-            return max ? 1 : -1;
-        }
-
-        if (s.size() >= 2 && this.roundNumber >= 4) {
-            int sum = top + s.get(s.size() - 2); // since stack has at least 2 elements and is at least round 4, create sum from top two numbers
-
-            if (sum >= 21) {
-                // win by combining the two top elements to sum
-                return max ? 1 : -1;
+        int top;
+        if (!s.isEmpty()) {
+            top = s.peek();
+            if (top >= 21) {
+                // check if position is already final (already >= 21)
+                return isMaximizingPlayersTurn ? -1000 : 1000;
             }
 
-            // check if stack is full
-            if (s.size() >= 6) {
-                if (top >= 12) {
-                    // win by adding large enough number on top
-                    return max ? 1 : -1;
+            if (s.size() >= 2 && this.roundNumber >= 4) {
+                int sum = top + s.get(s.size() - 2); // since stack has at least 2 elements and is at least round 4, create sum from top two numbers
+
+                if (sum >= 21) {
+                    // win by combining the two top elements to sum
+                    return isMaximizingPlayersTurn ? 100 : -100;
                 }
-                if (top == 11) {
-                    // cannot prevent enemy from winning on their next turn
-                    return max ? -1 : 1;
-                }
-                if (sum == 20) {
-                    // cannot prevent enemy from winning on their next turn
-                    return max ? -1 : 1;
+
+                // check if stack is full
+                if (currentStack.size() >= 6) {
+                    if (top >= 12) {
+                        // win by adding large enough number on top
+                        return isMaximizingPlayersTurn ? 100 : -100;
+                    }
+                    if (top == 11 || sum == 20) {
+                        // cannot prevent enemy from winning on their next turn
+                        return isMaximizingPlayersTurn ? -10 : 10;
+                    }
                 }
             }
+
         }
         // otherwise return 0 (neutral rating)
         return 0;
@@ -85,7 +86,7 @@ public class Position {
 
 
     public void simulatePossibleMoves() {
-        // if current player is not maximizing player (if it is player two) increment the round number for next position
+        // if current player is maximizing player (if it is player two, the AI) increment the round number for next position
         int newRoundNr = this.isMaximizingPlayer ? this.roundNumber + 1: this.roundNumber;
 
         for (int move = 0; move < 10; move++) {
@@ -95,8 +96,7 @@ public class Position {
             // simulate changes to stack depending on move made
             if (move == 0 && newStack.size() > 1 && this.roundNumber >= 4) {
                 newStack.push(newStack.pop() + newStack.pop());
-                Position child = new Position(newStack, newRoundNr, !this.isMaximizingPlayer, move);
-                children.add(child);
+                children.add(new Position(newStack, newRoundNr, !this.isMaximizingPlayer, move));
 
             } else if (move >= 1) {
                 if (newStack.size() == 6) {
@@ -106,8 +106,7 @@ public class Position {
                     newStack.push(move);
                 } // else { continue; }
 
-            Position child = new Position(newStack, newRoundNr, !this.isMaximizingPlayer, move);
-            children.add(child);
+            children.add(new Position(newStack, newRoundNr, !this.isMaximizingPlayer, move));
 
             }
         }
