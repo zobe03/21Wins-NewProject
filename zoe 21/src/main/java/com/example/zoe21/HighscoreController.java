75cfae952d8b040e3306c5e154e2492bca9ccdbc
Zoé <wi23032@lehre.dhbw-stuttgart.de>
2024-05-13 21:50:37 +0200

    hintergrund

--- zoe 21/src/main/java/com/example/zoe21/HighscoreController.java
+++ zoe 21/src/main/java/com/example/zoe21/HighscoreController.java
@@ -1,43 +1,56 @@
 package com.example.zoe21;
 
+import com.example.model.leaderboard.LeaderBoard;
+import com.example.model.leaderboard.LeaderBoardItem;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
 
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
-
 public class HighscoreController implements Initializable {
     @FXML
     protected Button backtomenu;
-
-    @FXML
-    protected void setBacktomenu(){
-        SwitchingScenes.setScene(0);
-    }
     @FXML
     private AnchorPane gameLayout;
     @FXML
-    public void initialize(URL location, ResourceBundle resources){
-        /*String imagePath = "file:resources/bücher3.jpeg";
-        Image image = new Image(imagePath);
-        BackgroundSize backgroundSize = new BackgroundSize(600, 600, true, true, false, true);
-        BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
-        gameLayout.setBackground(new Background(backgroundImage));
+    private Label highscoreLabel;
 
-         */
-          try {
-              InputStream is20 = MainController.class.getResourceAsStream("/font/PressStart2P-vaV7.ttf");
-              Font font = Font.loadFont(is20, 20);
-              backtomenu.setFont(font);
-          } catch (Exception e) {
-              e.printStackTrace();
-          }
+    @FXML
+    protected void setBacktomenu() {
+        SwitchingScenes.setScene(0);
+    }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Laden der Schriftart für den Button
+        try {
+            InputStream fontStream = getClass().getResourceAsStream("/font/PressStart2P-vaV7.ttf");
+            Font font = Font.loadFont(fontStream, 20);
+            backtomenu.setFont(font);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Laden des Highscores
+        displayHighscore();
+    }
+
+    // Methode zur Anzeige des Highscores
+    private void displayHighscore() {
+        LeaderBoard leaderBoard = new LeaderBoard();
+        leaderBoard.initializeFileManager(); // Laden des Leaderboards
+        StringBuilder highscoreText = new StringBuilder("Highscore:\n");
+        int rank = 1;
+        for (LeaderBoardItem item : leaderBoard.getItems()) {
+            highscoreText.append(rank).append(". ").append(item.getName()).append(": ").append(item.getFormatedScore()).append("\n");
+            rank++;
+        }
+        highscoreLabel.setText(highscoreText.toString());
     }
 }
--- zoe 21/src/main/java/com/example/zoe21/MainApplication.java
+++ zoe 21/src/main/java/com/example/zoe21/MainApplication.java
@@ -2,14 +2,12 @@ package com.example.zoe21;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -17,59 +15,57 @@ import java.io.IOException;
 
 // main program
 public class MainApplication extends Application {
-    Stage window;
-    Scene mainMenuScene;
-    Scene highscoreScene, regularGameScene, machineSetDifficulty;
 
+    private Stage window;
+    private Scene mainMenuScene, highscoreScene, regularGameScene, machineSetDifficultyScene;
 
     @Override
-    public void start(Stage stage) throws Exception {
-        // create the main menu
-
-
-
+    public void start(Stage stage) {
         window = stage;
 
-        // create main menu
-        MainController.SpaceBackground spaceBackgroundMenu = new MainController.SpaceBackground(800, 600);
-        FXMLLoader fxmlLoaderMenu = new FXMLLoader(MainApplication.class.getResource("mainmenu.fxml"));
-        mainMenuScene = new Scene(fxmlLoaderMenu.load(), 600, 600);
-        StackPane root = new StackPane();
-        root.getChildren().addAll((Node) spaceBackgroundMenu, mainMenuScene.getRoot());
-        mainMenuScene.setRoot(root);
-        stage.setScene(mainMenuScene);
-        stage.show();
+        // Erstelle die Szenen
+        createScenes();
 
-
-        // create various scenes + respective loader (size may be changed)
-
-        // highscore scene
-       // MainController.SpaceBackground spaceBackgroundHighscore = new MainController.SpaceBackground(800, 600);
-        FXMLLoader highscoreFxmlLoader = new FXMLLoader(MainApplication.class.getResource("highscore.fxml"));
-        highscoreScene = new Scene(highscoreFxmlLoader.load(), 600, 600);
-        //root.getChildren().addAll((Node) spaceBackgroundHighscore, highscoreScene.getRoot());
-        //highscoreScene.setRoot(root);
-
-
-
-        // regularGame (human players)
-        FXMLLoader regularGameFxmlLoader = new FXMLLoader(MainApplication.class.getResource("regularGame.fxml"));
-        regularGameScene = new Scene(regularGameFxmlLoader.load(), 600, 600);
-
-
-        // machineGame (human against player)
-        FXMLLoader machineSetDifficultyFxmlLoader = new FXMLLoader(MainApplication.class.getResource("machineSetDifficulty.fxml"));
-        machineSetDifficulty = new Scene(machineSetDifficultyFxmlLoader.load(), 600, 600);
-
-
-        SwitchingScenes.creatingSceneSwitcher(window, mainMenuScene, highscoreScene, regularGameScene,machineSetDifficulty);
+        // Setze die Hauptmenüszene als Startszene
         window.setScene(mainMenuScene);
         window.setTitle("21 Wins!!!");
         window.show();
+        SwitchingScenes.creatingSceneSwitcher(window, mainMenuScene, highscoreScene, regularGameScene, machineSetDifficultyScene);
+    }
 
+    private void createScenes() {
+        // Erstelle die Hintergrundobjekte
+        MainController.SpaceBackground spaceBackgroundMenu = new MainController.SpaceBackground(800, 600);
+        MainController.SpaceBackground spaceBackgroundHighscore = new MainController.SpaceBackground(800, 600);
+        MainController.SpaceBackground spaceBackgroundRegulargame = new MainController.SpaceBackground(800, 600);
+        MainController.SpaceBackground spaceBackgroundDifficulty = new MainController.SpaceBackground(800, 600);
+
+        // Lade die FXML-Dateien und erstelle die Szenen
+        try {
+            FXMLLoader fxmlLoaderMenu = new FXMLLoader(getClass().getResource("mainmenu.fxml"));
+            mainMenuScene = createSceneWithBackground(fxmlLoaderMenu.load(), spaceBackgroundMenu);
+
+            FXMLLoader highscoreFxmlLoader = new FXMLLoader(getClass().getResource("highscore.fxml"));
+            highscoreScene = createSceneWithBackground(highscoreFxmlLoader.load(), spaceBackgroundHighscore);
+
+            FXMLLoader regularGameFxmlLoader = new FXMLLoader(getClass().getResource("regularGame.fxml"));
+            regularGameScene = createSceneWithBackground(regularGameFxmlLoader.load(), spaceBackgroundRegulargame);
+
+            FXMLLoader machineSetDifficultyFxmlLoader = new FXMLLoader(getClass().getResource("machineSetDifficulty.fxml"));
+            machineSetDifficultyScene = createSceneWithBackground(machineSetDifficultyFxmlLoader.load(), spaceBackgroundDifficulty);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Scene createSceneWithBackground(Parent root, MainController.SpaceBackground background) {
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(background, root);
+        Scene scene = new Scene(stackPane, 600, 600);
+        return scene;
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
